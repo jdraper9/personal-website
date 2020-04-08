6582d69f0f3fb900c1721daaf34039014d9ea0d4
jdraper9 <jamesdraper24@gmail.com>
2020-04-07 20:50:49 -0700

    satellites working :)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,34 @@
 import React from 'react';
-import { PI } from '../constants';
+import { PI, satellite_init } from '../constants';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
-    // adapt this svg file
-    // define satellite init state.. will there be a problem with defining state with state? no, set initial conditions
-    //          update in updateSpheres
+    // initial conditions for 'planets' and 'satellites'
 
-    //insert satellites into respective arrays
     this.state = {
       rate: 1,
-      blue: {
+      outer: {
         r: 200,
         theta: 1.5 * PI,
         coordinates: { x: 250, y: 450 },
         period: 5000,
-        satellites: [],
+        satellites: satellite_init.outerSats,
       },
-      green: {
+      middle: {
         r: 125,
         theta: 0.5 * PI,
         coordinates: { x: 250, y: 125 },
         period: 3000,
-        satellites: [],
+        satellites: satellite_init.midSats,
       },
-      yellow: {
+      inner: {
         r: 50,
         theta: 1.5 * PI,
         coordinates: { x: 250, y: 300 },
         period: 1500,
-        satellites: [],
+        satellites: satellite_init.innerSats,
       },
     };
   }
@@ -43,9 +40,9 @@ class Home extends React.Component {
   rotate = () => {
     this.setState({
       rate: this.state.rate,
-      blue: this.updateSpheres(this.state.blue),
-      green: this.updateSpheres(this.state.green),
-      yellow: this.updateSpheres(this.state.yellow),
+      outer: this.updateSpheres(this.state.outer),
+      middle: this.updateSpheres(this.state.middle),
+      inner: this.updateSpheres(this.state.inner),
     });
   };
 
@@ -60,9 +57,37 @@ class Home extends React.Component {
     let x = r * Math.cos(newTheta),
       y = r * Math.sin(newTheta);
 
+    // object {x: , y: }
     let XY = this.convertCoordToSVGFrame(x, y);
-    return { r, theta: newTheta, coordinates: XY, period: fractionOfCircle };
+
+    // calc sat's here with XY
+    for (const sat in sphere.satellites) {
+      let updatedSat = this.updateSatellite(sphere.satellites[sat]);
+      sphere.satellites[sat] = updatedSat;
+    }
+
+    return {
+      r,
+      theta: newTheta,
+      coordinates: XY,
+      period: fractionOfCircle,
+      satellites: sphere.satellites,
+    };
   };
+
+  updateSatellite(sat) {
+    let newTheta = sat.theta - (2 * PI) / sat.period;
+
+    return {
+      r: sat.r,
+      theta: newTheta,
+      satDistance: {
+        x: sat.r * Math.cos(newTheta),
+        y: sat.r * Math.sin(newTheta),
+      },
+      period: sat.period,
+    };
+  }
 
   convertCoordToSVGFrame(x, y) {
     return { x: x + 250, y: -y + 250 };
@@ -71,6 +96,7 @@ class Home extends React.Component {
   render() {
     return (
       <svg
+        // seems like you can increase this and it scales
         width="500"
         height="500"
         viewBox="0 0 500 500"
@@ -81,259 +107,262 @@ class Home extends React.Component {
           <g id="outer">
             <g id="outer-ring-3">
               <circle
-                cx="253.5"
-                cy="450.5"
+                cx={this.state.outer.coordinates.x}
+                cy={this.state.outer.coordinates.y}
                 r="47.3"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="253.5"
-                cy="450.5"
-                r="47.3"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="out-sat-3"
-              cx="297.5"
-              cy="468.5"
+              cx={
+                this.state.outer.coordinates.x +
+                this.state.outer.satellites[3].satDistance.x
+              }
+              cy={
+                this.state.outer.coordinates.y +
+                this.state.outer.satellites[3].satDistance.y
+              }
               r="1.5"
-              fill="#3D413E"
+              fill="gray"
             />
             <g id="outer-ring-2">
               <circle
-                cx="253"
-                cy="450"
+                cx={this.state.outer.coordinates.x}
+                cy={this.state.outer.coordinates.y}
                 r="34.8"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="253"
-                cy="450"
-                r="34.8"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="out-sat-2"
-              cx="221.5"
-              cy="464.5"
+              cx={
+                this.state.outer.coordinates.x +
+                this.state.outer.satellites[2].satDistance.x
+              }
+              cy={
+                this.state.outer.coordinates.y +
+                this.state.outer.satellites[2].satDistance.y
+              }
               r="1.5"
-              fill="#3D413E"
+              fill="gray"
             />
-            <g id="outer-ring1">
+            <g id="outer-ring-1">
               <circle
-                cx="253"
-                cy="450"
+                cx={this.state.outer.coordinates.x}
+                cy={this.state.outer.coordinates.y}
                 r="24.8"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="253"
-                cy="450"
-                r="24.8"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="out-sat-1"
-              cx="253.5"
-              cy="425.5"
+              cx={
+                this.state.outer.coordinates.x +
+                this.state.outer.satellites[1].satDistance.x
+              }
+              cy={
+                this.state.outer.coordinates.y +
+                this.state.outer.satellites[1].satDistance.y
+              }
               r="1.5"
+              fill="gray"
+            />
+            {/* outer circle */}
+            <circle
+              id="outer"
+              cx={this.state.outer.coordinates.x}
+              cy={this.state.outer.coordinates.y}
+              r="10"
               fill="#3D413E"
             />
-            <circle id="blue" cx="253" cy="450" r="18" fill="#4E82E9" />
             <circle
               id="outer-orbit"
               cx="250"
               cy="250"
               r="199.5"
               stroke="black"
-              stroke-opacity="0.25"
+              strokeOpacity="0.25"
             />
           </g>
           <g id="mid">
             <g id="mid-ring-3">
               <circle
-                cx="249.5"
-                cy="126.5"
+                cx={this.state.middle.coordinates.x}
+                cy={this.state.middle.coordinates.y}
                 r="36.3"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="249.5"
-                cy="126.5"
-                r="36.3"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="mid-sat-3"
-              cx="249.5"
-              cy="90.5"
+              cx={
+                this.state.middle.coordinates.x +
+                this.state.middle.satellites[3].satDistance.x
+              }
+              cy={
+                this.state.middle.coordinates.y +
+                this.state.middle.satellites[3].satDistance.y
+              }
               r="1.5"
-              fill="#3D413E"
+              fill="gray"
             />
             <g id="mid-ring-2">
               <circle
-                cx="249.5"
-                cy="125.5"
+                cx={this.state.middle.coordinates.x}
+                cy={this.state.middle.coordinates.y}
                 r="32.3"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="249.5"
-                cy="125.5"
-                r="32.3"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="mid-sat-2"
-              cx="281.5"
-              cy="126.5"
+              cx={
+                this.state.middle.coordinates.x +
+                this.state.middle.satellites[2].satDistance.x
+              }
+              cy={
+                this.state.middle.coordinates.y +
+                this.state.middle.satellites[2].satDistance.y
+              }
               r="1.5"
-              fill="#3D413E"
+              fill="gray"
             />
             <g id="mid-ring-1">
               <circle
-                cx="250"
-                cy="126"
+                cx={this.state.middle.coordinates.x}
+                cy={this.state.middle.coordinates.y}
                 r="22.8"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="250"
-                cy="126"
-                r="22.8"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="mid-sat-1"
-              cx="227.5"
-              cy="126.5"
+              cx={
+                this.state.middle.coordinates.x +
+                this.state.middle.satellites[1].satDistance.x
+              }
+              cy={
+                this.state.middle.coordinates.y +
+                this.state.middle.satellites[1].satDistance.y
+              }
               r="1.5"
+              fill="gray"
+            />
+            {/* middle  */}
+            <circle
+              id="middle"
+              cx={this.state.middle.coordinates.x}
+              cy={this.state.middle.coordinates.y}
+              r="8"
               fill="#3D413E"
             />
-            <circle id="green" cx="249" cy="125" r="16" fill="#59BF76" />
             <circle
               id="middle-orbit"
               cx="250"
               cy="250"
               r="124.5"
               stroke="black"
-              stroke-opacity="0.25"
+              strokeOpacity="0.25"
             />
           </g>
           <g id="center">
             <g id="in-ring-3">
               <circle
-                cx="250"
-                cy="300"
+                cx={this.state.inner.coordinates.x}
+                cy={this.state.inner.coordinates.y}
                 r="34.8"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="250"
-                cy="300"
-                r="34.8"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="in-sat-3"
-              cx="215.5"
-              cy="298.5"
+              cx={
+                this.state.inner.coordinates.x +
+                this.state.inner.satellites[3].satDistance.x
+              }
+              cy={
+                this.state.inner.coordinates.y +
+                this.state.inner.satellites[3].satDistance.y
+              }
               r="1.5"
-              fill="#3D413E"
+              fill="gray"
             />
             <g id="in-ring-2">
               <circle
-                cx="250"
-                cy="300"
+                cx={this.state.inner.coordinates.x}
+                cy={this.state.inner.coordinates.y}
                 r="29.8"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="250"
-                cy="300"
-                r="29.8"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="in-sat-2"
-              cx="250.5"
-              cy="329.5"
+              cx={
+                this.state.inner.coordinates.x +
+                this.state.inner.satellites[2].satDistance.x
+              }
+              cy={
+                this.state.inner.coordinates.y +
+                this.state.inner.satellites[2].satDistance.y
+              }
               r="1.5"
-              fill="#3D413E"
+              fill="gray"
             />
             <g id="in-ring-1">
               <circle
-                cx="250"
-                cy="300"
+                cx={this.state.inner.coordinates.x}
+                cy={this.state.inner.coordinates.y}
                 r="19.8"
                 stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
-              />
-              <circle
-                cx="250"
-                cy="300"
-                r="19.8"
-                stroke="black"
-                stroke-opacity="0.05"
-                stroke-width="0.4"
+                strokeOpacity="0.05"
+                strokeWidth="0.4"
               />
             </g>
             <circle
               id="in-sat-1"
-              cx="263.5"
-              cy="285.5"
+              cx={
+                this.state.inner.coordinates.x +
+                this.state.inner.satellites[1].satDistance.x
+              }
+              cy={
+                this.state.inner.coordinates.y +
+                this.state.inner.satellites[1].satDistance.y
+              }
               r="1.5"
+              fill="gray"
+            />
+            {/* inner */}
+            <circle
+              id="inner"
+              cx={this.state.inner.coordinates.x}
+              cy={this.state.inner.coordinates.y}
+              r="6"
               fill="#3D413E"
             />
-            <circle id="yellow" cx="250" cy="300" r="15" fill="#DDE040" />
             <circle
               id="inner-orbit"
               cx="250"
               cy="250"
               r="49.5"
               stroke="black"
-              stroke-opacity="0.25"
+              strokeOpacity="0.25"
             />
           </g>
         </g>
@@ -343,59 +372,3 @@ class Home extends React.Component {
 }
 
 export default Home;
-
-// <svg
-//         width="500"
-//         height="500"
-//         viewBox="0 0 500 500"
-//         fill="none"
-//         xmlns="http://www.w3.org/2000/svg"
-//       >
-//         <g id="planets_2">
-//           <circle
-//             id="outer-orbit"
-//             cx="250"
-//             cy="250"
-//             r="200"
-//             stroke="black"
-//             strokeOpacity="0.25"
-//           />
-//           <circle
-//             id="middle-orbit"
-//             cx="250"
-//             cy="250"
-//             r="125"
-//             stroke="black"
-//             strokeOpacity="0.25"
-//           />
-//           <circle
-//             id="inner-orbit"
-//             cx="250"
-//             cy="250"
-//             r="50"
-//             stroke="black"
-//             strokeOpacity="0.25"
-//           />
-//           <circle
-//             className="outer-circle"
-//             cx={this.state.blue.coordinates.x}
-//             cy={this.state.blue.coordinates.y}
-//             r="10"
-//             fill="gray"
-//           />
-//           <circle
-//             id="mid-circle"
-//             cx={this.state.green.coordinates.x}
-//             cy={this.state.green.coordinates.y}
-//             r="8"
-//             fill="gray"
-//           />
-//           <circle
-//             className="inner-circle"
-//             cx={this.state.yellow.coordinates.x}
-//             cy={this.state.yellow.coordinates.y}
-//             r="6"
-//             fill="gray"
-//           />
-//         </g>
-//       </svg>
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,64 @@
 export const PI = Math.PI;
+
+export const satellite_init = {
+  outerSats: {
+    1: {
+      r: 24.8,
+      theta: 0.5 * PI,
+      satDistance: { x: 0, y: 24.8 },
+      period: 400,
+    },
+    2: {
+      r: 34.8,
+      theta: (4 / 3) * PI,
+      satDistance: { x: -17.4, y: -30.14 },
+      period: 600,
+    },
+    3: {
+      r: 47.3,
+      theta: (5 / 3) * PI,
+      satDistance: { x: 23.65, y: -40.96 },
+      period: 800,
+    },
+  },
+  midSats: {
+    1: {
+      r: 22.8,
+      theta: PI,
+      satDistance: { x: -22.8, y: 0 },
+      period: 300,
+    },
+    2: {
+      r: 32.3,
+      theta: 0,
+      satDistance: { x: 32.3, y: 0 },
+      period: 600,
+    },
+    3: {
+      r: 36.3,
+      theta: 0.5 * PI,
+      satDistance: { x: 0, y: 36.3 },
+      period: 900,
+    },
+  },
+  innerSats: {
+    1: {
+      r: 19.8,
+      theta: (1 / 3) * PI,
+      satDistance: { x: 9.9, y: 17.15 },
+      period: 400,
+    },
+    2: {
+      r: 29.8,
+      theta: (3 / 2) * PI,
+      satDistance: { x: 0, y: -29.8 },
+      period: 650,
+    },
+    3: {
+      r: 34.8,
+      theta: PI,
+      satDistance: { x: -34.8, y: 0 },
+      period: 800,
+    },
+  },
+};
