f80c4ab486f69c2e682e11e36fbfbd0f83ef487b
jdraper9 <jamesdraper24@gmail.com>
2020-04-13 12:40:46 -0700

    zoom in/out working great, changed colors, figured out opening link in new tab. Next, figure out adding text about projects, and intro animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { PI, satellite_init } from '../constants';
 
 class Home extends React.Component {
-  intervlID = 0;
+  intervalID = 0;
+  zoomIntervalID = 0;
+
   constructor(props) {
     super(props);
 
@@ -12,7 +14,13 @@ class Home extends React.Component {
       rate: 1,
       selected: null,
       clickedAndSelected: false,
+      zoomTimer: 0,
+      zoomedIn: false,
+      zoomedInBox: null,
       zoomBox: '0 0 500 500',
+      orbitRingOpacity: 0.25,
+      notSunOpacity: 1,
+      clickedOnSun: false,
       outer: {
         r: 200,
         theta: 1.5 * PI,
@@ -21,18 +29,22 @@ class Home extends React.Component {
         satellites: satellite_init.outerSats,
       },
       middle: {
-        r: 125,
+        r: 130,
         theta: 0.5 * PI,
         coordinates: { x: 250, y: 125 },
         period: 3000,
         satellites: satellite_init.midSats,
       },
       inner: {
-        r: 50,
+        r: 60,
         theta: 1.5 * PI,
         coordinates: { x: 250, y: 300 },
         period: 1500,
         satellites: satellite_init.innerSats,
+      },
+      sun: {
+        coordinates: { x: 250, y: 250 },
+        satellites: { 3: { r: 30 } },
       },
     };
   }
@@ -60,12 +72,26 @@ class Home extends React.Component {
     });
   };
 
-  // when a planet group is selected, zoom, etc
+  // when a planet group is clicked, zoom, etc. need clickedAndSelected in state so planets stop rotating
   select = () => {
     let clickedAndSelected = this.state.selected !== null ? true : false;
-    var viewBoxString = '0 0 500 500';
+    this.setState({ clickedAndSelected });
     // group of planet and satellites (outer, middle, inner) need coordinates, and largest radius of satellite
+
+    if (this.state.zoomedIn && clickedAndSelected) {
+      // redirect or toggle about
+      // switch?
+      if (this.state.selected !== 'sun') {
+        window.open('https://www.google.com', 'newtab');
+      } else {
+        console.log('hey');
+      }
+    }
+
     if (clickedAndSelected) {
+      if (this.state.selected === 'sun') {
+        this.setState({ clickedOnSun: true });
+      }
       let selectedOrbit = this.state[this.state.selected];
       let largestSatRadius = Math.round(selectedOrbit.satellites[3].r);
       let x = Math.round(selectedOrbit.coordinates.x),
@@ -75,10 +101,94 @@ class Home extends React.Component {
         width = 2 * largestSatRadius + 20,
         height = 2 * largestSatRadius + 20;
 
-      viewBoxString = `${viewMinX} ${viewMinY} ${width} ${height} `;
+      // zoom in
+      this.zoomIntervalID = setInterval(
+        () =>
+          this.zoomIntervalFunction(
+            0,
+            0,
+            500,
+            500,
+            viewMinX,
+            viewMinY,
+            width,
+            height
+          ),
+        1
+      );
+      // zoom out
+    } else if (this.state.zoomedIn) {
+      this.zoomIntervalID = setInterval(
+        () =>
+          this.zoomIntervalFunction(
+            0,
+            0,
+            500,
+            500,
+            this.state.zoomedInBox.x,
+            this.state.zoomedInBox.y,
+            this.state.zoomedInBox.w,
+            this.state.zoomedInBox.h
+          ),
+        1
+      );
+    } else {
     }
-    this.setState({ clickedAndSelected, zoomBox: viewBoxString });
   };
+
+  zoomIntervalFunction = (x0, y0, w0, h0, x1, y1, w1, h1) => {
+    if (this.state.zoomTimer === 201) {
+      this.setState({
+        zoomTimer: 200,
+        zoomedIn: true,
+        zoomedInBox: { x: x1, y: y1, w: w1, h: h1 },
+      });
+      clearInterval(this.zoomIntervalID);
+    } else if (this.state.zoomTimer === -1 && this.state.zoomedIn) {
+      this.setState({
+        zoomTimer: 0,
+        zoomedIn: false,
+        zoomedInBox: null,
+        clickedOnSun: false,
+        notSunOpacity: 1,
+      });
+      clearInterval(this.zoomIntervalID);
+    } else {
+      let zoomBoxString = this.incrementZoom(
+        x0,
+        y0,
+        w0,
+        h0,
+        x1,
+        y1,
+        w1,
+        h1,
+        this.state.zoomTimer / 200
+      );
+      this.setState({
+        zoomTimer: !this.state.zoomedIn
+          ? this.state.zoomTimer + 1
+          : this.state.zoomTimer - 1,
+        zoomBox: zoomBoxString,
+        orbitRingOpacity: !this.state.zoomedIn
+          ? this.state.orbitRingOpacity - (1 / 200) * 0.25
+          : this.state.orbitRingOpacity + (1 / 200) * 0.25,
+        notSunOpacity:
+          !this.state.zoomedIn && this.state.clickedOnSun
+            ? this.state.notSunOpacity - (1 / 200) * 1
+            : this.state.notSunOpacity + (1 / 200) * 1,
+      });
+    }
+  };
+
+  incrementZoom(x0, y0, w0, h0, x1, y1, w1, h1, segmentFraction) {
+    let newX = (x1 - x0) * segmentFraction + x0,
+      newY = (y1 - y0) * segmentFraction + y0,
+      newW = (w1 - w0) * segmentFraction + w0,
+      newH = (h1 - h0) * segmentFraction + h0;
+
+    return `${newX} ${newY} ${newW} ${newH}`;
+  }
 
   rotate = () => {
     this.setState({
@@ -150,19 +260,15 @@ class Home extends React.Component {
         xmlns="http://www.w3.org/2000/svg"
         onClick={this.select}
       >
-        <g id="planets_3">
+        <g id="planets_3" opacity={this.state.notSunOpacity}>
           {/*  */}
 
-          <g
-            id="outer"
-            onMouseEnter={() => this.slow({ title: 'outer', rate: 60 })}
-            onMouseLeave={this.fast}
-          >
+          <g id="outer">
             <g id="outer-ring-3">
               <circle
                 cx={this.state.outer.coordinates.x}
                 cy={this.state.outer.coordinates.y}
-                r="47.3"
+                r="36"
                 stroke="black"
                 strokeOpacity="0.13"
                 strokeWidth="0.4"
@@ -185,7 +291,7 @@ class Home extends React.Component {
               <circle
                 cx={this.state.outer.coordinates.x}
                 cy={this.state.outer.coordinates.y}
-                r="34.8"
+                r="30"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -208,7 +314,7 @@ class Home extends React.Component {
               <circle
                 cx={this.state.outer.coordinates.x}
                 cy={this.state.outer.coordinates.y}
-                r="24.8"
+                r="22"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -235,29 +341,28 @@ class Home extends React.Component {
               cy="250"
               r="199.5"
               stroke={this.state.selected === 'outer' ? 'blue' : 'black'}
-              strokeOpacity="0.25"
+              strokeOpacity={`${this.state.orbitRingOpacity}`}
             />
             <circle
               id="outer"
               cx={this.state.outer.coordinates.x}
               cy={this.state.outer.coordinates.y}
-              r="10"
-              fill="#3D413E"
+              r="9"
+              fill="#4074B1"
+              opacity=".95"
+              onMouseEnter={() => this.slow({ title: 'outer', rate: 60 })}
+              onMouseLeave={this.fast}
             />
           </g>
 
           {/*  */}
 
-          <g
-            id="mid"
-            onMouseEnter={() => this.slow({ title: 'middle', rate: 70 })}
-            onMouseLeave={this.fast}
-          >
+          <g id="mid">
             <g id="mid-ring-3">
               <circle
                 cx={this.state.middle.coordinates.x}
                 cy={this.state.middle.coordinates.y}
-                r="36.3"
+                r="30"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -280,7 +385,7 @@ class Home extends React.Component {
               <circle
                 cx={this.state.middle.coordinates.x}
                 cy={this.state.middle.coordinates.y}
-                r="32.3"
+                r="22"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -303,7 +408,7 @@ class Home extends React.Component {
               <circle
                 cx={this.state.middle.coordinates.x}
                 cy={this.state.middle.coordinates.y}
-                r="22.8"
+                r="16"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -327,31 +432,30 @@ class Home extends React.Component {
               id="middle-orbit"
               cx="250"
               cy="250"
-              r="124.5"
-              stroke={this.state.selected === 'middle' ? 'green' : 'black'}
-              strokeOpacity="0.25"
+              r="130"
+              stroke={this.state.selected === 'middle' ? 'orange' : 'black'}
+              strokeOpacity={`${this.state.orbitRingOpacity}`}
             />
             <circle
               id="middle"
               cx={this.state.middle.coordinates.x}
               cy={this.state.middle.coordinates.y}
               r="8"
-              fill="#3D413E"
+              fill="#A54F00"
+              opacity=".95"
+              onMouseEnter={() => this.slow({ title: 'middle', rate: 70 })}
+              onMouseLeave={this.fast}
             />
           </g>
 
           {/*  */}
 
-          <g
-            id="center"
-            onMouseEnter={() => this.slow({ title: 'inner', rate: 90 })}
-            onMouseLeave={this.fast}
-          >
+          <g id="center">
             <g id="in-ring-3">
               <circle
                 cx={this.state.inner.coordinates.x}
                 cy={this.state.inner.coordinates.y}
-                r="34.8"
+                r="25"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -367,14 +471,14 @@ class Home extends React.Component {
                 this.state.inner.coordinates.y +
                 this.state.inner.satellites[3].satDistance.y
               }
-              r="1.5"
+              r="1"
               fill="gray"
             />
             <g id="in-ring-2">
               <circle
                 cx={this.state.inner.coordinates.x}
                 cy={this.state.inner.coordinates.y}
-                r="29.8"
+                r="20"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -390,14 +494,14 @@ class Home extends React.Component {
                 this.state.inner.coordinates.y +
                 this.state.inner.satellites[2].satDistance.y
               }
-              r="1.5"
+              r="1"
               fill="gray"
             />
             <g id="in-ring-1">
               <circle
                 cx={this.state.inner.coordinates.x}
                 cy={this.state.inner.coordinates.y}
-                r="19.8"
+                r="14"
                 stroke="black"
                 strokeOpacity="00.13"
                 strokeWidth="0.4"
@@ -413,7 +517,7 @@ class Home extends React.Component {
                 this.state.inner.coordinates.y +
                 this.state.inner.satellites[1].satDistance.y
               }
-              r="1.5"
+              r="1"
               fill="gray"
             />
             {/* inner */}
@@ -421,19 +525,115 @@ class Home extends React.Component {
               id="inner-orbit"
               cx="250"
               cy="250"
-              r="49.5"
-              stroke={this.state.selected === 'inner' ? 'purple' : 'black'}
-              strokeOpacity="0.25"
+              r="60"
+              stroke={this.state.selected === 'inner' ? 'green' : 'black'}
+              strokeOpacity={`${this.state.orbitRingOpacity}`}
             />
             <circle
               id="inner"
               cx={this.state.inner.coordinates.x}
               cy={this.state.inner.coordinates.y}
               r="6"
-              fill="#3D413E"
+              fill="#177431"
+              onMouseEnter={() => this.slow({ title: 'inner', rate: 90 })}
+              onMouseLeave={this.fast}
+              opacity=".95"
             />
           </g>
         </g>
+        <g
+          id="intro-and-sun"
+          onMouseEnter={() => this.slow({ title: 'sun', rate: 3 })}
+          onMouseLeave={this.fast}
+        >
+          <g id="sun">
+            <circle cx="250" cy="250" r="30" fill="url(#paint0_radial)" />
+          </g>
+          <g id="intro-sats">
+            <circle
+              id="teeny-tiny9"
+              cx="265.5"
+              cy="268.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny8"
+              cx="249.5"
+              cy="263.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny7"
+              cx="270.5"
+              cy="245.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny6"
+              cx="226.5"
+              cy="246.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny5"
+              cx="234.5"
+              cy="236.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny4"
+              cx="263.5"
+              cy="233.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny3"
+              cx="254.5"
+              cy="246.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny2"
+              cx="236.5"
+              cy="268.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+            <circle
+              id="teeny-tiny1"
+              cx="244.5"
+              cy="249.5"
+              r="1.5"
+              fill="#3D413E"
+            />
+          </g>
+          <g id="intro-planets">
+            <circle id="yellow" cx="235.5" cy="255.5" r="5.5" fill="#A54F00" />
+            <circle id="blue_2" cx="262" cy="257" r="5" fill="#4074B1" />
+            <circle id="green_2" cx="249" cy="234" r="6" fill="#177431" />
+          </g>
+        </g>
+        <defs>
+          <radialGradient
+            id="paint0_radial"
+            cx="0"
+            cy="0"
+            r="1"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(250 250) rotate(90) scale(30)"
+          >
+            <stop stopColor="#FFB03A" />
+            <stop offset="1" stopColor="#F50C0C" stopOpacity="0" />
+            <stop offset="1" stopColor="#FFF73A" stopOpacity="0" />
+          </radialGradient>
+        </defs>
       </svg>
     );
   }
--- src/constants.js
+++ src/constants.js
@@ -3,19 +3,19 @@ export const PI = Math.PI;
 export const satellite_init = {
   outerSats: {
     1: {
-      r: 24.8,
+      r: 22,
       theta: 0.5 * PI,
       satDistance: { x: 0, y: 24.8 },
       period: 400,
     },
     2: {
-      r: 34.8,
+      r: 30,
       theta: (4 / 3) * PI,
       satDistance: { x: -17.4, y: -30.14 },
       period: 600,
     },
     3: {
-      r: 47.3,
+      r: 36,
       theta: (5 / 3) * PI,
       satDistance: { x: 23.65, y: -40.96 },
       period: 800,
@@ -23,19 +23,19 @@ export const satellite_init = {
   },
   midSats: {
     1: {
-      r: 22.8,
+      r: 16,
       theta: PI,
       satDistance: { x: -22.8, y: 0 },
-      period: 300,
+      period: 450,
     },
     2: {
-      r: 32.3,
+      r: 22,
       theta: 0,
       satDistance: { x: 32.3, y: 0 },
       period: 600,
     },
     3: {
-      r: 36.3,
+      r: 30,
       theta: 0.5 * PI,
       satDistance: { x: 0, y: 36.3 },
       period: 900,
@@ -43,19 +43,19 @@ export const satellite_init = {
   },
   innerSats: {
     1: {
-      r: 19.8,
+      r: 14,
       theta: (1 / 3) * PI,
       satDistance: { x: 9.9, y: 17.15 },
       period: 400,
     },
     2: {
-      r: 29.8,
+      r: 20,
       theta: (3 / 2) * PI,
       satDistance: { x: 0, y: -29.8 },
       period: 650,
     },
     3: {
-      r: 34.8,
+      r: 25,
       theta: PI,
       satDistance: { x: -34.8, y: 0 },
       period: 800,
