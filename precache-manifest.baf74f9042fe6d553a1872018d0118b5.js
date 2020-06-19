f0648a48522ce9ddf3d6f51ee1eebe7de28b529b
jdraper9 <jamesdraper24@gmail.com>
2020-06-19 14:01:38 -0700

    Updates

---
+++ precache-manifest.baf74f9042fe6d553a1872018d0118b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741f6efdb260a7882ba82efa115d0d90",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "bdae9fd5338f4a146b49",
+    "url": "/personal-website/static/css/main.f75e55d7.chunk.css"
+  },
+  {
+    "revision": "0e99e32f95a88e08a1ea",
+    "url": "/personal-website/static/js/2.d17d1620.chunk.js"
+  },
+  {
+    "revision": "73dcaa3dc28586a6e161ef363892b265",
+    "url": "/personal-website/static/js/2.d17d1620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdae9fd5338f4a146b49",
+    "url": "/personal-website/static/js/main.bc2a989e.chunk.js"
+  },
+  {
+    "revision": "08f6d01088b4496f4b5d",
+    "url": "/personal-website/static/js/runtime-main.7c08e48f.js"
+  },
+  {
+    "revision": "bcba47f8071033c930d4f5997de29332",
+    "url": "/personal-website/static/media/prof_pic.bcba47f8.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e147ea6534408494005b66c52cc4056a.js"
+  "/personal-website/precache-manifest.baf74f9042fe6d553a1872018d0118b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
